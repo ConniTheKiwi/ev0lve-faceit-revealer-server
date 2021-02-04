85c795fd29e43c425065a6860bb71ff3a731889f
Conni <connithekiwi@gmail.com>
2021-02-04 02:11:56 +0000

    Update FaceItOut.lua

--- script/versions/0.0.0.1/FaceItOut.lua
+++ script/versions/0.0.0.1/FaceItOut.lua
@@ -1,21 +1,15 @@
 local api_gateway       = gui.new_textbox("API Gateway", "faceit_dumper_api_gateway")
 local api_path          = gui.new_textbox("API Path", "faceit_deumper_api_path")
 local apiKey_t          = gui.new_textbox("API Key", "faceit_dumper_apiKey")
-local steam_32_t        = gui.new_textbox("steam_32", "faceit_dumper_steam_32")
 
 local output_in_chat    = gui.new_checkbox("In Chat", "faceit_dumper_in_chat", false)
 
-api_gateway:set_value("http://localhost:80/api/0/faceit")
-api_path:set_value("/userinfo/output")
-apiKey_t:set_value("API KEY") -- Excluding "Bearer "
-steam_32_t:set_value("76561199050201019")
+api_gateway:set_value("http://localhost:80/api/0/faceit")  -- Should be fine on default
+api_path:set_value("/userinfo/output")                     -- Should be fine on default
+apiKey_t:set_value("FACE IT API KEY") -- Excluding "Bearer " - https://developers.faceit.com/
 
 print("Loaded.")
 
--- http.get(api:get_value(), {
---     steam_32 = steam_32_t:get_value(),
---     apiKey = apiKey_t:get_value()
--- }, on_get)
 
 local ready = false
 
@@ -66,17 +60,14 @@ new_button("Everyone test", function(self)
     if processing == true then
         print("Please wait for last scan to finish.")
     end
-
     if processing == false then
         if engine_client.is_connected()then
             if engine_client.is_ingame() then
                 processing = true
-
                 print("-----------------")
                 print("Starting scan")
                 print("-----------------")
                 for i = 1, entity_list.get_highest_index() do
-                    -- sendChatMessage("Ent Count: "..tostring(i))
                     if ready == false then
                         i = i - 1
                     end
@@ -85,20 +76,13 @@ new_button("Everyone test", function(self)
 
                     if ent:is_valid() then
                         local info2 = ent:get_player_info()
-                        --print("-----------------")
                         if info2.steam_id ~= "BOT" then
-
-                            --print(info2.name)
-                            --print(info2.steam_id64)
-                        -- print("-----------------")
                             makeReq(info2.steam_id64)
                         end
                     end
-                    -- makeReq(steam_32_t:get_value())
                 end 
                 processing = false
                 print("Finished.")
-
             end
         end
     end
